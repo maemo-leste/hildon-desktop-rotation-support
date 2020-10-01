3c906e86f9f1b4c2df807cfb030af897a5e7bcd5
Merlijn Wajer <merlijn@wizzup.org>
2020-10-02 01:24:24 +0200

    Initial commit

---
+++ scripts/usr/bin/hildon-desktop-rotate-touchscreen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Needed because hildon sends DesktopOrientationChanged before it actually
+# rotates the display
+
+sleep 1
+
+export DISPLAY=:0
+
+tsids=$(xinput --list | awk -v search="Touch" \
+    '$0 ~ search {match($0, /id=[0-9]+/);\
+                  if (RSTART) \
+                    print substr($0, RSTART+3, RLENGTH-3)\
+                 }'\
+     )
+
+ctrc=$(xrandr | awk '/ connected/{print $1}' | awk 'NR==1 {print; exit}')
+
+for i in $tsids
+do
+        xinput map-to-output $i $ctrc
+done
